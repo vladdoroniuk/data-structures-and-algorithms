4bdbfd4b424ffd4c30c41f169f18d98d51b947f4
vladdoroniuk <vladyslav.doroniuk@gmail.com>
2023-02-04 22:51:11 +0200

    feat: add 'Selection sort' algorithm

--- src/bubble-sort/bubble-sort.test.ts
+++ src/bubble-sort/bubble-sort.test.ts
@@ -3,16 +3,21 @@ import { bubble_sort, bubble_sort_optimized } from "./bubble-sort";
 
 describe.concurrent("Bubble sort", () => {
   let sorted_array: number[];
+  let shuffled_array: number[];
   let reversed_array: number[];
 
   beforeEach(() => {
     sorted_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    shuffled_array = [6, 2, 8, 9, 3, 4, 7, 1, 5, 10];
     reversed_array = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
   });
 
   it("sorts array with the 'bubble_sort' function", () => {
     bubble_sort(sorted_array);
     expect(sorted_array).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+    bubble_sort(shuffled_array);
+    expect(shuffled_array).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 
     bubble_sort(reversed_array);
     expect(reversed_array).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
@@ -21,6 +26,9 @@ describe.concurrent("Bubble sort", () => {
   it("sorts array with the 'bubble_sort_optimized' function", () => {
     bubble_sort_optimized(sorted_array);
     expect(sorted_array).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+    bubble_sort(shuffled_array);
+    expect(shuffled_array).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 
     bubble_sort_optimized(reversed_array);
     expect(reversed_array).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
---
+++ src/selection-sort/README.md
@@ -0,0 +1,5 @@
+# About
+
+| Algorithm      | Best-case time complexity          | Average-case time complexity       | Worst-case time complexity         | Worst-case space complexity |
+| -------------- | ---------------------------------- | ---------------------------------- | ---------------------------------- | --------------------------- |
+| Selection sort | $O(n^2)$ comparisons, $O(1)$ swaps | $O(n^2)$ comparisons, $O(n)$ swaps | $O(n^2)$ comparisons, $O(n)$ swaps | $O(1)$                      |
