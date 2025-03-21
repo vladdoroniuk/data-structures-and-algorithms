519dfba2e3e8f8d5b0d3ecb5fdf9c4350130abb1
vladdoroniuk <vladyslav.doroniuk@gmail.com>
2025-03-21 20:15:29 +0200

    feat: add binary search

--- binary_search/binary_search_test.go
+++ binary_search/binary_search_test.go
@@ -1,1 +1,24 @@
 package binarysearch
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestBinarySearchInt(t *testing.T) {
+	ints := []int{1, 3, 5, 7, 9, 11}
+
+	assert.Equal(t, 2, BinarySearch(ints, 5))
+}
+
+func TestBinarySearchString(t *testing.T) {
+	strings := []string{"apple", "banana", "cherry", "date", "fig", "grape"}
+
+	assert.Equal(t, 5, BinarySearch(strings, "grape"))
+}
+
+func TestBinarySearchFloat(t *testing.T) {
+	floats := []float64{1.1, 2.2, 3.3, 4.4, 5.5, 6.6}
+
+	assert.Equal(t, 3, BinarySearch(floats, 4.4))
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/vladdoroniuk/data-structures-and-algorithms
 
-go 1.21.1
+go 1.24
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.10.0 // indirect
+	golang.org/x/exp v0.0.0-20250305212735-054e65f0b394 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
