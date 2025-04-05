8d71ad1526c2289efd9aec0fc0151348ed033b6b
vladdoroniuk <vladyslav.doroniuk@gmail.com>
2025-04-05 21:19:31 +0300

    feat: add bubble sort

--- bubble_sort/bubble_sort_optimized.go
+++ bubble_sort/bubble_sort_optimized.go
@@ -2,16 +2,23 @@ package bubble_sort
 
 import "cmp"
 
-func BubbleSort[S ~[]E, E cmp.Ordered](slice S) S {
+func BubbleSortOptimized[S ~[]E, E cmp.Ordered](slice S) S {
 	if len(slice) < 2 {
 		return slice
 	}
 
 	for i := 0; i < len(slice)-1; i++ {
+		swapped := false
+
 		for j := 0; j < len(slice)-1-i; j++ {
 			if slice[j] > slice[j+1] {
 				slice[j], slice[j+1] = slice[j+1], slice[j]
+				swapped = true
 			}
+		}
+
+		if !swapped {
+			break
 		}
 	}
 
---
+++ bubble_sort/bubble_sort_optimized_test.go
@@ -0,0 +1,60 @@
+package bubble_sort
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestBubbleSortOptimizedInt(t *testing.T) {
+	ints := []int{5, 3, 9, 1, 7}
+	expected := []int{1, 3, 5, 7, 9}
+
+	assert.Equal(t, expected, BubbleSortOptimized(ints))
+}
+
+func TestBubbleSortOptimizedString(t *testing.T) {
+	strings := []string{"banana", "apple", "date", "cherry", "fig"}
+	expected := []string{"apple", "banana", "cherry", "date", "fig"}
+
+	assert.Equal(t, expected, BubbleSortOptimized(strings))
+}
+
+func TestBubbleSortOptimizedFloat(t *testing.T) {
+	floats := []float64{3.3, 1.1, 5.5, 4.4, 2.2}
+	expected := []float64{1.1, 2.2, 3.3, 4.4, 5.5}
+
+	assert.Equal(t, expected, BubbleSortOptimized(floats))
+}
+
+func TestBubbleSortOptimizedEmpty(t *testing.T) {
+	var empty []int
+	var expected []int
+
+	assert.Equal(t, expected, BubbleSortOptimized(empty))
+}
+
+func TestBubbleSortOptimizedSingleElement(t *testing.T) {
+	single := []int{42}
+	expected := []int{42}
+
+	assert.Equal(t, expected, BubbleSortOptimized(single))
+}
+
+func TestBubbleSortOptimizedAlreadySorted(t *testing.T) {
+	sorted := []int{1, 2, 3, 4, 5}
+	expected := []int{1, 2, 3, 4, 5}
+
+	assert.Equal(t, expected, BubbleSortOptimized(sorted))
+}
+
+func TestBubbleSortOptimizedReverseSorted(t *testing.T) {
+	reverse := []int{5, 4, 3, 2, 1}
+	expected := []int{1, 2, 3, 4, 5}
+
+	assert.Equal(t, expected, BubbleSortOptimized(reverse))
+}
+
+func TestBubbleSortOptimizedNilSlice(t *testing.T) {
+	var nilSlice []int
+	assert.Nil(t, BubbleSortOptimized(nilSlice))
+}
